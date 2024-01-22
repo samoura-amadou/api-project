d95b438978424ecdd8b8be7191498ef57d9c78dd
samoura <samoura.amadou@gmail.com>
2024-01-22 03:52:46 +0100

    update

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port :  ${PORT}`));
